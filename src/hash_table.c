3b577a30e8df997c5f7e8df8348f497d3c8b7e8b
me <me@someemail.com>
2020-04-28 16:52:23 +0100

    Add methods to Hash table

--- src/hash_table.c
+++ src/hash_table.c
@@ -4,6 +4,13 @@
 
 #include "hash_table.h"
 
+#define HT_PRIME_1 139
+#define HT_PRIME_2 149
+
+// todo: do not use strcmp() use strncmp()
+
+static ht_item HT_DELETED_ITEM = {NULL, NULL};
+
 static ht_item* ht_new_item(const char* k, const char* v)
 {
 	ht_item* i = malloc(sizeof(ht_item));
@@ -58,4 +65,63 @@ static int ht_get_hash(
 	const int hash_a = ht_hash(s, HT_PRIME_1, num_buckets);
 	const int hash_b = ht_hash(s, HT_PRIME_2, num_buckets);
 	return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
+}
+
+void ht_insert(ht_hash_table* ht, const char* key, const char* value)
+{
+	ht_item* item = ht_new_item(key, value);
+	int index = ht_get_hash(item->key, ht->size, 0);
+	ht_item* cur_item = ht->items[index];
+	int i = 1;
+	while(cur_item != NULL){
+		if(cur_item != &HT_DELETED_ITEM){
+			if(strcmp(cur_item->key, key) == 0){
+				ht_del_item(cur_item);
+				ht->items[index] = item;
+				return;
+			}
+		}
+		index = ht_get_hash(item->key, ht->size, i);
+		cur_item = ht->items[index];
+		i++;
+	}
+	ht->items[index] = item;
+	ht->count++;
+}
+
+char* ht_search(ht_hash_table* ht, const char* key)
+{
+	int index = ht_get_hash(key, ht->size, 0);
+	ht_item* item = ht->items[index];
+	int i = 1;
+	while (item != NULL) {
+		if(item != &HT_DELETED_ITEM){
+			if (strcmp(item->key, key) == 0){
+				return item->value;
+			}
+		}
+		index = ht_get_hash(key, ht->size, i);
+		item = ht->items[index];
+		i++;
+	}
+	return NULL;
+}
+
+void ht_delete(ht_hash_table* ht, const char* key)
+{
+	int index = ht_get_hash(key, ht->size, 0);
+	ht_item* item = ht->items[index];
+	int i = 1;
+	while (item != NULL){
+		if (item != &HT_DELETED_ITEM){
+			if(strcmp(item->key, key) == 0){
+				ht_del_item(item);
+				ht->items[index] = &HT_DELETED_ITEM;
+			}
+		}
+		index = ht_get_hash(key, ht->size, i);
+		item = ht->items[index];
+		i++;
+	}
+	ht->count--; // todo: this might cause a bug when ht == NULL
 }

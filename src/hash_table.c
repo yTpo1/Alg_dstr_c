1359026898bfae81f9ef656bcf0e69385f5909d4
me <me@someemail.com>
2020-04-28 14:33:35 +0100

    Setting up the Hash function

--- src/hash_table.c
+++ src/hash_table.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #include "hash_table.h"
 
@@ -39,3 +40,15 @@ void ht_del_hash_table(ht_hash_table* ht)
 	free(ht->items);
 	free(ht);
 }
+
+static int ht_hash(const char* s, const int a, const int m)
+{
+	long hash = 0;
+	const int len_s = strlen(s);
+	for (int i = 0; i< len_s; i++) {
+		hash += (long)pow(a, len_s - (i+1)) * s[i];
+		hash = hash % m;
+	}
+	return (int)hash;
+}
+
